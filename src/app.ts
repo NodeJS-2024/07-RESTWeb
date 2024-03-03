fffb0736e811a7916270cf9f616e6bd74442269c
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-02 20:21:40 -0600

    Primer rest server

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,15 @@
+import http from 'http';
 
-console.log('Hola Mundo');
+const server = http.createServer((req, res) => {
+
+  console.log(req.url);
+
+  res.write('Hola Mundo');
+  res.end();
+
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+})
+
