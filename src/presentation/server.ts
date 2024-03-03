c9cd2f06f6e2bab000a66bc512803baeabf5ef31
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-03 12:14:15 -0600

    Uso de variables de entorno con dotenv y env-var

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,22 @@
 import express from 'express';
 import path from 'path';
 
+interface Options {
+  port: number,
+  public_path?: string,
+}
+
 export class Server {
 
   private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+
+  constructor(options: Options) {
+    const { port, public_path = 'public' } = options;
+    this.port = port;
+    this.publicPath = public_path;    
+  }
 
   async start() {
 
@@ -11,20 +24,20 @@ export class Server {
 
     //* Public Folder
     // Incluimos el webserver a mostrar
-    this.app.use(express.static('public'));
+    this.app.use(express.static(this.publicPath));
     
     this.app.get('*', (req, res) => {
       // console.log(req.url);
       // res.send('Hola Mundo');
 
       // Resolvemos lo de recargar la pagina que no sea la ruta principal
-      const indexPath = path.join(__dirname + '../../../public/index.html');
+      const indexPath = path.join(__dirname + `../../../${ this.publicPath }/index.html`);
       res.sendFile(indexPath);
       return;
     });
 
-    this.app.listen(3000, () => {
-      console.log(`Server running on port ${ 3000 }`);
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${ this.port }`);
     });
 
   }
