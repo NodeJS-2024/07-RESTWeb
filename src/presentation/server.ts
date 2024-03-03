60ed058e7b7598f381d303b9077a8a92ad823da3
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-03 11:50:02 -0600

    Servir SPA con Router

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export class Server {
 
@@ -9,7 +10,18 @@ export class Server {
     //* Middlewares
 
     //* Public Folder
+    // Incluimos el webserver a mostrar
     this.app.use(express.static('public'));
+    
+    this.app.get('*', (req, res) => {
+      // console.log(req.url);
+      // res.send('Hola Mundo');
+
+      // Resolvemos lo de recargar la pagina que no sea la ruta principal
+      const indexPath = path.join(__dirname + '../../../public/index.html');
+      res.sendFile(indexPath);
+      return;
+    });
 
     this.app.listen(3000, () => {
       console.log(`Server running on port ${ 3000 }`);
