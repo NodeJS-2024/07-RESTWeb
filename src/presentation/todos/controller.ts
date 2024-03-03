90c1fef211c8edb34370e206258b981d303a40c0
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-03 13:55:54 -0600

    Actualizar todo con metodo PUT

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 
 const todos = [
-  { id: 1, text: 'Buy milk', createdAt: new Date() },
-  { id: 2, text: 'Buy bread', createdAt: null },
-  { id: 3, text: 'Buy butter', createdAt: new Date() },
+  { id: 1, text: 'Buy milk', completedAt: new Date() },
+  { id: 2, text: 'Buy bread', completedAt: null },
+  { id: 3, text: 'Buy butter', completedAt: new Date() },
 ];
 
 export class TodoController {
@@ -12,7 +12,7 @@ export class TodoController {
   constructor() {}
 
   public getTodos = (req: Request, res: Response) => {
-    return res.json(todos);
+    return res.status(200).json(todos);
   }
 
   public getTodoById = (req: Request, res: Response) => {
@@ -24,7 +24,7 @@ export class TodoController {
     const todo = todos.find(todo => todo.id === id);
 
     ( todo )
-      ? res.json(todo)
+      ? res.status(200).json(todo)
       : res.status(404).json({ error: `TODO with ${ id } not found` });
 
   }
@@ -37,12 +37,36 @@ export class TodoController {
     const newTodo = {
       id: todos.length + 1,
       text: text,
-      createdAt: new Date(),
+      completedAt: new Date(),
     }
 
     todos.push(newTodo);
 
-    return res.json(newTodo);
+    return res.status(201).json(newTodo);
+
+  }
+
+  public updateTodo = (req: Request, res: Response) => {
+
+    const id = +req.params.id;
+
+    if ( isNaN(id) ) return res.status(400).json({ error: `ID argument is not a number` });
+
+    const todo = todos.find(todo => todo.id === id);
+    if ( !todo ) return res.status(404).json({ error: `Todo with id ${ id } not found` });
+
+    const { text, completedAt } = req.body;
+    // if ( !text ) return res.status(400).json({ error: 'Text property is required' });
+
+    //! paso por referencia
+    // todo.text = text;
+
+    todo.text = text || todo.text;
+    ( completedAt === 'null' )
+      ? todo.completedAt = null
+      : todo.completedAt = new Date( completedAt || todo.completedAt );
+
+    return res.status(200).json(todo);
 
   }
 
