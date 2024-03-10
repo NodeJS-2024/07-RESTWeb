1a3bf766fcae3d6ca59a09612f9bbd37a3e8676d
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-10 13:27:32 -0600

    TodoDataSource implementacion

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -9,7 +9,27 @@ export class TodoEntity {
 
   get isCompleted() {
     return !!this.completedAt;
-  } 
+  }
+
+  public static fromObject(object: { [key: string]: any }): TodoEntity {
+    const { id, text, completedAt } = object;
+
+    if (!id) throw 'Id is required';
+    if (!text) throw 'text is required';
+
+    let newCompletedAt;
+
+    // Validar una fecha
+    if (completedAt) {
+      newCompletedAt = new Date(completedAt);
+
+      if (isNaN(newCompletedAt.getTime())) {
+        throw 'CompletedAt is no a valid date';
+      }
+    }
+
+    return new TodoEntity(id, text, completedAt);
+  }
 
 }
 
