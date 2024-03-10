1005b44f800cc88f92417e74ba243c4aa96caa67
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-10 16:53:48 -0600

    Prueba al obtener todos los TODOs get

--- test/presentation/todos/routes.test.ts
+++ test/presentation/todos/routes.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { testServer } from '../../test-server';
+import { prisma } from '../../../src/data/postgres';
 
 describe('Todo route testing', () => {
 
@@ -10,14 +11,26 @@ describe('Todo route testing', () => {
   afterAll(() => {
     testServer.close();
   });
+
+  const todo1 = { text: 'Hola Mundo 1' };
+  const todo2 = { text: 'Hola Mundo 2' };
   
   test('should return TODOs api/todos', async () => {
 
-    const response = await request(testServer.app)
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({
+      data: [ todo1, todo2 ]
+    });
+
+    const { body } = await request(testServer.app)
       .get('/api/todos')
       .expect(200);
 
-    console.log(response.body);
+    expect( body ).toBeInstanceOf( Array );
+    expect( body.length ).toBe( 2 );
+    expect( body[0].text ).toBe( todo1.text );
+    expect( body[1].text ).toBe( todo2.text );
+    expect( body[0].completedAt ).toBeNull();
 
   });
 
