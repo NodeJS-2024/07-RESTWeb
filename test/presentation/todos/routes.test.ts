32c574e25b651e4d186ed56b79f1f1cdf3c3d02d
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-10 17:03:21 -0600

    Test al obtener un todo rest get

--- test/presentation/todos/routes.test.ts
+++ test/presentation/todos/routes.test.ts
@@ -12,12 +12,15 @@ describe('Todo route testing', () => {
     testServer.close();
   });
 
+  beforeEach(async() => {
+    await prisma.todo.deleteMany();
+  });
+
   const todo1 = { text: 'Hola Mundo 1' };
   const todo2 = { text: 'Hola Mundo 2' };
   
   test('should return TODOs api/todos', async () => {
 
-    await prisma.todo.deleteMany();
     await prisma.todo.createMany({
       data: [ todo1, todo2 ]
     });
@@ -31,6 +34,22 @@ describe('Todo route testing', () => {
     expect( body[0].text ).toBe( todo1.text );
     expect( body[1].text ).toBe( todo2.text );
     expect( body[0].completedAt ).toBeNull();
+
+  });
+
+  test('should return TODOs api/todos/:id', async () => {
+
+    const todo = await prisma.todo.create({ data: todo1 });
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${ todo.id }`)
+      .expect(200);
+
+    expect(body).toEqual({
+      id: todo.id,
+      text: todo.text,
+      completedAt: todo.completedAt
+    });
 
   });
 
