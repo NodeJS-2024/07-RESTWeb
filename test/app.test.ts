91f2de101093d3801c8d07a11dafa67422618439
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-10 16:11:30 -0600

    Pruebas en app.ts con cambio de variables de entorno a env.test y mock

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,22 @@
+import { envs } from '../src/config/envs';
+import { Server } from '../src/presentation/server';
+
+jest.mock('../src/presentation/server'); // Todo lo que esta en ese archivo es un mock
 
 describe('Testing App.ts', () => {
   
-  test('should work', () => {
+  test('should call server with arguments and start', async () => {
 
-    expect(true).toBeTruthy();
+    await import('../src/app');
+
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      public_path: envs.PUBLIC_PATH,
+      routes: expect.any(Function),
+    });
+
+    expect(Server.prototype.start).toHaveBeenCalledWith();
 
   });
 
