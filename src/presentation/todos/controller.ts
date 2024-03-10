fe99a7b8e19a798fd1c8224410a899d96c125242
Luis Angel <angelfgdeveloper@gmail.com>
2024-03-10 14:19:45 -0600

    Uso del repositorio en los controladores

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express';
 import { prisma } from '../../data/postgres';
 import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos';
+import { TodoRepository } from '../../domain';
 
 // Prisma esta altamente acomplado en los controllers
 export class TodoController {
 
   //* DI => Inyeccion de dependencias
-  constructor() {}
+  constructor(
+    private readonly todoRepository: TodoRepository
+  ) {}
 
   public getTodos = async (req: Request, res: Response) => {
-    const todos = await prisma.todo.findMany();
+    const todos = await this.todoRepository.getAll();
     return res.status(200).json(todos);
   }
 
@@ -17,78 +20,42 @@ export class TodoController {
 
     const id = +req.params.id;
 
-    if ( isNaN(id) ) return res.status(400).json({ error: `ID argument is not a number` });
-
-    const todo = await prisma.todo.findFirst({
-      where: {
-        id: id
-      }
-    });
-
-    ( todo )
-      ? res.status(200).json(todo)
-      : res.status(404).json({ error: `TODO with ${ id } not found` });
+    try {
+      const todo = await this.todoRepository.findById(id);
+      return res.status(200).json(todo);
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
 
   }
 
   public createTodo = async (req: Request, res: Response) => {
-
     const [ error, createTodoDto ] = CreateTodoDto.create(req.body);
 
     if ( error ) return res.status(400).json({ error });
 
-    const todo = await prisma.todo.create({
-      data: createTodoDto!
-    });
+    const todo = await this.todoRepository.create(createTodoDto!);
 
     return res.status(201).json(todo);
-
   }
 
   public updateTodo = async (req: Request, res: Response) => {
-
     const id = +req.params.id;
 
     const [ error, updateTodoDto ] = UpdateTodoDto.create({ ...req.body, id });
     if ( error ) return res.status(400).json({ error});
 
-    const todo = await prisma.todo.findFirst({ where: { id } });
-
-    if ( !todo ) return res.status(404).json({ error: `Todo with id ${ id } not found` });
-
-    const updatedTodo = await prisma.todo.update({
-      where: { id },
-      data: updateTodoDto!.values
-    });
+    const updatedTodo = await this.todoRepository.updateById(updateTodoDto!);
 
     return res.status(200).json(updatedTodo);
-
   }
 
   public deleteTodo = async (req: Request, res: Response) => {
-
     const id = +req.params.id;
 
-    if ( isNaN(id) ) return res.status(400).json({ error: `ID argument is not a number` });
+    const deletedTodo = await this.todoRepository.deleteById(id);
 
-    const todo = await prisma.todo.findFirst({
-      where: {
-        id: id
-      }
-    });
-
-    if ( !todo ) return res.status(404).json({ error: `Todo with id ${ id } not found` });
-    
-    const deleted = await prisma.todo.delete({
-      where: {
-        id: id
-      }
-    });
-
-    ( deleted )
-      ? res.status(200).json(deleted)
-      : res.status(400).json({ error: `Todo with id ${ id } not found` });
-
+    return res.status(200).json(deletedTodo);
   }
 
 }
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { TodoController } from './controller';
+import { TodoDataSourceImpl } from '../../infrastructure/datasources/todo.datasource';
+import { TodoRepositoryImpl } from '../../infrastructure/repositories/todo.repository';
 
 export class TodoRoutes {
 
   static get routes(): Router {
     const router = Router();
-    const todoController = new TodoController();
+
+    // Integrando la Inyeccionde dependecias
+    const datasource = new TodoDataSourceImpl();
+    const todoRepository = new TodoRepositoryImpl(datasource);
+    const todoController = new TodoController(todoRepository);
 
     router.get('/', todoController.getTodos);
     router.get('/:id', todoController.getTodoById);
